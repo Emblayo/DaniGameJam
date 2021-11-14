8c059eafa780f5200cf5078788ee7184edb97043
Snov <snowconure@gmail.com>
2021-11-14 18:18:25 +0100

    levels + dragon

---
+++ Pleasework/Assets/Dragon.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dragon : Enemy
+{
+    public float AttackSpeed;
+    private float currTime;
+
+    public GameObject fireBall;
+    public float fireBallSpeed;
+
+    private void Update()
+    {
+        currTime += Time.deltaTime;
+        if (currTime > AttackSpeed)
+        {
+            Attack();
+            currTime = 0;
+        }
+    }
+
+    void Attack()
+    {
+        GameObject fireBallInstance = (GameObject)Instantiate(fireBall, new Vector2(transform.position.x - .4f, transform.position.y + .5f), transform.rotation);
+
+        fireBallInstance.GetComponent<Rigidbody2D>().velocity = Vector2.left * fireBallSpeed;
+    }
+}
--- Pleasework/Assets/Scripts/DamageHandler.cs
+++ Pleasework/Assets/Scripts/DamageHandler.cs
@@ -36,6 +36,9 @@ public class DamageHandler : MonoBehaviour
             case "Enemy":
                 TakeDamage();
                 break;
+            case "FireBall":
+                TakeDamage();
+                break;
         }
     }
 
