c06932cfcd478eea6a9d2cf382ceebf330ca2517
Snov <snowconure@gmail.com>
2021-11-14 16:15:06 +0100

    Enemy hit feed back

--- Pleasework/Assets/Scripts/Enemy.cs
+++ Pleasework/Assets/Scripts/Enemy.cs
@@ -6,12 +6,24 @@ public class Enemy : MonoBehaviour, IDamageable
 {
     public float health;
 
+    [HideInInspector] public bool canTakeKnockback = true;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void IDamageable.TakeDamage(float dmg)
     {
         health -= dmg;
 
         //check if dead
         if (health <= 0) Die();
+
+        //apply knockback
+        rb.AddForce((Vector2.up * 5) + (Vector2.right * 5), ForceMode2D.Impulse);
     }
 
     void Die()
